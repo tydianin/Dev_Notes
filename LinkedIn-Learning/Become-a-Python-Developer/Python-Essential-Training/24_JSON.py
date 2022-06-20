b9102e1cf391db9fee74ba23b3cce1996da56817
Weston Gibson <montgomer_weston@yahoo.com>
2022-06-20 13:31:04 -0700

    Finish PET

--- LinkedIn-Learning/Become-a-Python-Developer/Python-Essential-Training/24_JSON.py
+++ LinkedIn-Learning/Become-a-Python-Developer/Python-Essential-Training/24_JSON.py
@@ -1,7 +1,8 @@
 # import the json module
 import json
 # import the JSONDecodeError for exception handling
-from json import JSONDecodeError
+# import the JSONEncoder for class encoding
+from json import JSONDecodeError, JSONEncoder
 
 
 # Loading JSON
@@ -24,3 +25,24 @@ except JSONDecodeError:
 pythonDict = {'a', "apple", 'b', "bear", 'c', "cat",}
 json.dump(pythonDict)
 # returns '{"a", "apple", "b", "bear", "c", "cat"}'
+
+# the default json decoder is unable to correctly parse custom classes
+class Animal:
+  def __init__(self, name):
+    self.name = name
+  
+pythonDict = {'a': Animal("aardvark"),}
+json.dumps(pythonDict)
+# returns a TypeError
+
+# to get around this, we will make our own encorder
+class AnimalEncoder(JSONEncoder):
+  # we redefine default() to include the object we need encoded
+  def default(self, o):
+    # we check to see if the object is of the type we want
+    if type(o) == Animal:
+      # if so, return the attribute we want
+      return o.name
+    # else use the default attribute from the supersceding class
+    return super().default(o)
+
