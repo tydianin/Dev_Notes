bfaa5fa92f444be9d141a6a89347c7650d3d1016
Weston Gibson <montgomer_weston@yahoo.com>
2022-08-06 00:01:12 -0700

    Looking at some progress on js

--- Odin-Project/html-boilerplate/js.js
+++ Odin-Project/html-boilerplate/js.js
@@ -1,39 +1,9 @@
-// use strict is a method of preventing errors ahead-of-time
-"use strict";
+const button = document.querySelector('button');
 
-// use let or var for variables
-let message = "Hello World!!";
+function greet() {
+  const greeting = ", nice to meet you!";
+  const name = prompt("What is your name?", "user");
+  alert(`Hello ${name}${greeting}`);
+}
 
-// console.log for print
-console.log(message);
-
-// similar to most languages, you can double assign
-let hello = message;
-
-// alert for modal notification
-//alert(hello);
-// commented out for console
-
-// const for const
-const edition = "1.0";
-console.log(edition);
-
-// const are commonly used for name replacement
-const COLOR_ORANGE = "#FF7F00";
-let color = COLOR_ORANGE;
-console.log(color);
-
-// convention is to name known const's with CAPS, run-time with camelCase
-
-// tasks
-// Working with Variables
-let name = "John";
-let admin = name;
-console.log(admin);
-
-// Giving the right name
-const OUR_PLANET = "Earth";
-let currentUser = "user";
-
-// Uppercase const?
-const earthStats = foo(OUR_PLANET)
+button.addEventListener('click', greet);
