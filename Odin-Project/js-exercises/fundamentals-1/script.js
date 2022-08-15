93ab3159cea4d9790f9afa863bd601eb6a5d59ba
Weston Gibson <montgomer_weston@yahoo.com>
2022-08-14 23:55:17 -0700

    Complete FizzBuzz

---
+++ Odin-Project/js-exercises/fundamentals-1/script.js
@@ -0,0 +1,25 @@
+// testing
+function testing() {
+  console.log("Hello World!!");
+}
+
+// task 1
+function task1() {
+  let name = "John",
+    admin = name;
+
+  console.log(admin);
+}
+
+// task 2
+function task2() {
+  const SUN_PLANET_3 = "Earth";
+  let currentUser = "Jane";
+}
+
+// task 3
+function task3() {
+  let a = +"1";
+  let b = +"2";
+  console.log(a + b);
+}
