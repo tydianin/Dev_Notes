39b31c3d00d9722a0fc9ef75f50c3a19563c24a0
Weston LapPop! <89497650+tydianin@users.noreply.github.com>
2023-05-04 17:14:04 -0700

    Falling Asleep

---
+++ Odin-Project/js-exercises/fundamentals-part-2/Conditionals/Mozilla-Developers/conditionals_1.js
@@ -0,0 +1,12 @@
+let season = 'summer';
+let response;
+
+// Add your code here
+
+// Don't edit the code below here!
+
+const section = document.querySelector('section');
+
+let para1 = document.createElement('p');
+para1.textContent = response;
+section.appendChild(para1);
